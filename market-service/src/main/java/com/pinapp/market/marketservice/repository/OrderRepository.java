5afc8318f3c4625b4600c7b9d2e3cfa2ed02e42e
Joel Martinez <85506032+Banneeddm@users.noreply.github.com>
2021-11-17 18:06:32 -0300

    Update OrderRepository.java

--- market-service/src/main/java/com/pinapp/market/marketservice/repository/OrderRepository.java
+++ market-service/src/main/java/com/pinapp/market/marketservice/repository/OrderRepository.java
@@ -1,12 +1,12 @@
-package main.java.com.pinapp.market.marketservice.repository;
+package com.pinapp.market.marketservice.repository;
 
-import com.pinapp.market.marketservice.domain.model.Order;
+import com.pinapp.market.marketservice.domain.model.SaleNote;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface OrderRepository extends CrudRepository<Order, Long>{
+public interface OrderRepository extends CrudRepository<SaleNote, Long>{
     
     
 }
